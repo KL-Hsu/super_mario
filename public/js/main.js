79b1edf3f8be4b72de199150d403eb8f3df01fa9
klhsu <allen.klhsu@gmail.com>
2021-09-01 00:34:18 +0800

    create the background

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,27 @@
-function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', ()=> {
-            resolve(image);
-        });
-        image.src = url;
-    });
-}
+import SpriteSheet from './SpriteSheet.js';
+import {loadImage} from './lorders.js'
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 context.fillRect(0, 0, 50, 50);
 
+loadImage('/img/tiles.png')
+.then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('ground', 0, 0);
+    sprites.define('sky', 3, 23);
+
+    for (let x = 0; x < 25; ++x) {
+        for (let y = 0; y < 14; ++y) {
+            sprites.drawTile('sky', context, x, y);
+        }
+    }
+
+    for (let x = 0; x < 25; ++x) {
+        for (let y = 12; y < 14; ++y) {
+            sprites.drawTile('ground', context, x, y);
+        }
+    }
+
+});
